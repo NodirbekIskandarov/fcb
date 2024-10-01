136eab102d02f4db7e491480128e621bb8c0e345
NodirbekIskandarov <nodirbekiskandarov2003@gmail.com>
2024-10-01 13:50:36 +0500

    api begginning

---
+++ src/components/allGamers/AllGamers.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react'
+import './allGamers.scss'
+import { getRequest } from '../../utils/request'
+import { all_players } from '../../utils/API_urls'
+function AllGamers() {
+  useEffect(() => {
+    getRequest(all_players)
+    .then(response => {
+      console.log(response, "response oyinchilar")
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }, [])
+  return (
+    <div className='all_gamers'>
+      hamma oyinchilar uchun joy
+    </div>
+  )
+}
+
+export default AllGamers
---
+++ src/utils/API_urls.js
@@ -0,0 +1,2 @@
+export const host = process.env.REACT_APP_API_URL ?? 'https://admin.ellikqala.uz'
+export const all_players = "all/"
